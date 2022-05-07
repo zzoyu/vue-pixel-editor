1687f2408ccad5e5971872f0df427ddff6a190d4
zzoyu <zzoyu@hotmail.com>
2022-05-08 01:01:02 +0900

    chore: :memo: add link to my github

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,5 +1,10 @@
 <template>
   <h1 class="m-3 max-w-fit">
-    Hello, 8-bit! <small class="underline">github</small>
+    Hello, 8-bit!
+    <small class="underline">
+      <a href="https://github.com/zzoyu/vue-pixel-editor" target="blank"
+        >github
+      </a>
+    </small>
   </h1>
 </template>
