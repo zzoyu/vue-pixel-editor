af4fd25c35b6c100078575423251f2aa8117a859
zzoyu <zzoyu@hotmail.com>
2022-05-08 04:23:33 +0900

    feat: :necktie: add selection to palette

--- src/components/sidebar/ThePalette.vue
+++ src/components/sidebar/ThePalette.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
+import { computed, watch } from "vue";
 import { useStore } from "../../stores";
 
 import BasePane from "../UI/BasePane.vue";
 import PaletteItem from "./PaletteItem.vue";
+
 const store = useStore();
+const selectedPalette = computed(() => store.selectedPalette);
+watch(selectedPalette, () => {
+  store.selectedColor = 0;
+});
 </script>
 
 <template>
   <BasePane>
     <template #title>PALETTE</template>
-    <h3>{{ store.palette.name }}</h3>
-    <small>by {{ store.palette.author }}</small>
+
+    <h3>{{ store.palette[selectedPalette].name }}</h3>
+    <small>by {{ store.palette[selectedPalette].author }}</small>
     <div class="grid grid-flow-row grid-cols-9 gap-1 max-w-fit">
       <PaletteItem
-        v-for="(color, index) in store.palette"
+        v-for="(color, index) in store.palette[selectedPalette]"
         @click.self="store.selectedColor = index"
         :color="color"
         :selected="store.selectedColor === index"
         :key="color.hex"
       ></PaletteItem>
     </div>
+    <select class="mt-3 max-w-max" v-model="store.selectedPalette">
+      <option
+        v-for="(palette, index) in store.palette"
+        :key="palette.name"
+        :value="index"
+      >
+        {{ palette.name }}
+      </option>
+    </select>
   </BasePane>
 </template>
 
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import Palette from "../classes/palette";
 
 import ArcadeStandard29 from "../data/ARCADE_STANDARD_29.json";
+import PICO8 from "../data/PICO-8.json";
 
 // useStore could be anything like useUser, useCart
 // the first argument is a unique id of the store across your application
@@ -9,11 +10,14 @@ export const useStore = defineStore("index", {
   // other options...
 
   state: () => {
-    const palette = Palette.fromJSON(ArcadeStandard29);
+    const palette = new Array<Palette>();
+    palette.push(Palette.fromJSON(ArcadeStandard29));
+    palette.push(Palette.fromJSON(PICO8));
 
     return {
       scale: 0,
       palette: palette,
+      selectedPalette: 0,
       selectedColor: 0,
     };
   },
