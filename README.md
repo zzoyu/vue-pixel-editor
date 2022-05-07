d9e540fbe944e172a449facf947786a30589241a
zzoyu <zzoyu@hotmail.com>
2022-05-08 02:54:45 +0900

    feat: :heavy_plus_sign: add pinia

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pixel editor
 
-Vue 3 + TypeScript + Vite + TailwindCSS 기반의 픽셀 에디터입니다.
+Vue 3 + TypeScript + Vite + Pinia + TailwindCSS 기반의 픽셀 에디터입니다.
 레트로 풍의 게임 개발에 사용되는 스프라이트 작업을 위하여 제작하고 있습니다.
 
 아직 개발 작업중입니다.
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import "@kfonts/neodgm-code";
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+createApp(App).use(createPinia()).mount("#app");
