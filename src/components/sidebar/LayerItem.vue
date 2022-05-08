f7d369bac22270345c47b65653050a82ac53fa61
zzoyu <zzoyu@hotmail.com>
2022-05-08 17:44:01 +0900

    feat: :necktie: add layer config and drag layers to change priority

---
+++ src/components/sidebar/LayerItem.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Layer } from "../../classes/layer";
+import { Icon } from "@iconify/vue";
+import debounce from "lodash/debounce";
+
+const isDragging = ref(false);
+
+defineProps({
+  data: {
+    type: Layer,
+    required: true,
+    default: null,
+  },
+});
+const emit = defineEmits<{
+  (event: "deleteLayer"): void;
+  (event: "startDrag"): void;
+  (event: "moveLayer", offset: number): void;
+  (event: "endDrag"): void;
+}>();
+
+const deleteLayer = () => {
+  if (confirm?.("해당 레이어를 삭제하시겠습니까?")) emit("deleteLayer");
+};
+
+const startMove = (event: MouseEvent) => {
+  if (isDragging.value) return;
+  isDragging.value = true;
+  window.addEventListener("mouseup", endMove);
+  window.addEventListener("mousemove", moveItem);
+};
+
+const endMove = () => {
+  isDragging.value = false;
+  window.removeEventListener("mouseup", endMove);
+};
+
+const moveItem = debounce((event: MouseEvent) => {
+  if (!isDragging.value) return;
+
+  const rowRect = row.value?.getBoundingClientRect();
+
+  if (event.clientY > rowRect!.bottom) {
+    emit(
+      "moveLayer",
+      Math.ceil((event.clientY - rowRect!.bottom) / rowRect!.height)
+    );
+  } else if (event.clientY < rowRect!.top) {
+    emit(
+      "moveLayer",
+      -Math.ceil((rowRect!.top - event.clientY) / rowRect!.height)
+    );
+  }
+}, 10);
+
+const row = ref<HTMLElement>();
+</script>
+
+<template>
+  <div
+    class="flex flex-row justify-between px-1 py-1 items-center"
+    ref="row"
+    :class="{ 'opacity-50 cursor-move': isDragging }"
+  >
+    <button class="cursor-move" @mousedown="startMove">
+      <Icon icon="pixelarticons:menu" />
+    </button>
+    <h3
+      class="max-w-fit flex flex-row items-center"
+      :class="{ 'line-through': !data.isVisible }"
+    >
+      {{ data.name
+      }}<small v-if="data.isLocked"><Icon icon="pixelarticons:lock" /></small>
+    </h3>
+    <div class="grid grid-flow-col gap-1 items-center">
+      <button v-if="data.isVisible" @click.stop="data.hide()">
+        <Icon icon="pixelarticons:eye" />
+      </button>
+      <button v-else @click.stop="data.show()">
+        <Icon icon="pixelarticons:eye-closed" />
+      </button>
+      <button v-if="data.isLocked" @click.stop="data.unLock()">
+        <Icon icon="pixelarticons:lock" />
+      </button>
+      <button v-else @click.stop="data.lock()">
+        <Icon icon="pixelarticons:lock-open" />
+      </button>
+      <button @click.stop="deleteLayer">
+        <Icon icon="pixelarticons:trash" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/sidebar/TheLayer.vue
+++ src/components/sidebar/TheLayer.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
+import { Icon } from "@iconify/vue";
 import { ref } from "vue";
+import { useStore } from "../../stores";
+
 import BasePane from "../UI/BasePane.vue";
+import LayerItem from "./LayerItem.vue";
+
+const store = useStore();
 </script>
 
 <template>
   <BasePane>
     <template #title>LAYER</template>
-    <h1>Hello, Layer!</h1>
+    <template #button>
+      <button v-if="store.isLayerVisible" @click.stop="store.hideAll()">
+        <Icon icon="pixelarticons:eye" />
+      </button>
+      <button v-else @click.stop="store.showAll()">
+        <Icon icon="pixelarticons:eye-closed" />
+      </button>
+    </template>
+    <!-- <h1>Hello, Layer!</h1> -->
+    <div class="divide-y divide-slate-300">
+      <LayerItem
+        v-for="(layer, index) in store.layer"
+        :key="layer.name"
+        :data="layer"
+        @delete-layer="store.deleteLayer(index)"
+        @move-layer="(offset:number)=>store.exchangeLayerIndex(index, index + offset)"
+      ></LayerItem>
+    </div>
   </BasePane>
 </template>
 
