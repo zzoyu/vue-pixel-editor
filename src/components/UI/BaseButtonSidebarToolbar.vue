959d0fc0ae00f07a182e87f96a0dbf1c3f690bea
zzoyu <zzoyu@hotmail.com>
2022-06-25 23:17:21 +0900

    feat(command): :sparkles: add tooltip of command

--- src/components/UI/BaseButtonSidebarToolbar.vue
+++ src/components/UI/BaseButtonSidebarToolbar.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
+import { ref } from "vue";
 import { Command } from "../../classes/command";
 
 const props = defineProps({
   command: { type: Command, required: true },
   isActive: { type: Boolean, required: false, default: false },
 });
+
+const isMouseHovered = ref(false);
 </script>
 
 <template>
   <button
+    @mouseenter="isMouseHovered = true"
+    @mouseleave="isMouseHovered = false"
     class="w-8 h-8 outline outline-1 outline-slate-300 rounded-sm flex justify-center items-center bg-slate-50"
     :class="{
       'outline-double outline-4 z-10': isActive,
@@ -17,5 +22,10 @@ const props = defineProps({
     }"
   >
     <Icon :icon="`pixelarticons:${command.icon}`" />
+    <span
+      v-if="isMouseHovered"
+      class="absolute outline outline-1 bg-white w-max ml-8 translate-x-1/2"
+      >{{ command.name }}</span
+    >
   </button>
 </template>
