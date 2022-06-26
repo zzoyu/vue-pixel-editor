1a6f4cf7c0285735c47789a795fb7bb0aea5a3fe
zzoyu <zzoyu@hotmail.com>
2022-06-26 23:58:16 +0900

    feat(command): :sparkles: add filling area command with dfs

--- src/classes/layer.ts
+++ src/classes/layer.ts
@@ -1,3 +1,4 @@
+import Color from "./color";
 import Drawable from "./drawable";
 import Pixel from "./pixel";
 
@@ -8,6 +9,8 @@ export class Layer extends Drawable {
   isLocked: boolean;
   name: string;
   id: number;
+  width: number;
+  height: number;
   static index: number = 0;
 
   constructor(
@@ -15,6 +18,9 @@ export class Layer extends Drawable {
     index: number = Layer.index + 1
   ) {
     super();
+
+    this.width = size.width;
+    this.height = size.height;
 
     // 캔버스 크기만큼 레이어 생성
     this.pixels = new Array<Array<Pixel | null>>(size.height)
@@ -68,6 +74,64 @@ export class Layer extends Drawable {
 
   removePixel(x: number, y: number) {
     delete this.pixels[y][x];
+  }
+
+  fillPixel(x: number, y: number, color: Color) {
+    if (isNaN(x) || isNaN(y)) return;
+    const threshold = this.pixels[y][x]?.color; // 같은 색상은 채우기 처리
+
+    const directions = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
+
+    const nextPositions = new Array<{ x: number; y: number }>();
+    const visitedPositions = new Array<{ x: number; y: number }>();
+
+    nextPositions.push({ x, y }); // 출발 위치 지정
+
+    while (nextPositions.length !== 0) {
+      console.log("DFS");
+      const currentPosition = nextPositions.pop() as { x: number; y: number };
+      visitedPositions.push(currentPosition);
+      this.pixels[currentPosition.y][currentPosition.x] = new Pixel(
+        color,
+        currentPosition.x,
+        currentPosition.y
+      );
+
+      for (const direction of directions) {
+        const checkingPosition = {
+          x: currentPosition!.x + direction[0],
+          y: currentPosition!.y + direction[1],
+        };
+
+        // 바운더리 체크
+        if (checkingPosition.x >= this.width || checkingPosition.x < 0)
+          continue;
+        if (checkingPosition.y >= this.height || checkingPosition.y < 0)
+          continue;
+
+        const checkingPixel =
+          this.pixels[checkingPosition.y][checkingPosition.x];
+
+        // 빈 공간에 채우기를 시도했거나 최초 시작점과 같은 색상인지, 방문했는지 체크
+        if (
+          visitedPositions.find(
+            (i) => i.x === checkingPosition.x && i.y === checkingPosition.y
+          )
+        )
+          continue;
+        if (
+          (!threshold && !checkingPixel) ||
+          checkingPixel?.color.hex === threshold?.hex
+        ) {
+          nextPositions.push(checkingPosition);
+        } else visitedPositions.push(checkingPosition);
+      }
+    }
   }
 
   render(context: CanvasRenderingContext2D, scale: number): void {
